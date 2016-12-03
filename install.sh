a53baeee019976032b0d545832db5101408d8906
Dan Lange <metaliSPAMfloyd@gmail.com>
2016-12-03 11:57:02 -0800

    Install Vundle if needed.

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-echo $DOTFILES_DIR
+VUNDLE_DIR="$DOTFILES_DIR/.vim/bundle/Vundle.vim"
+if [ ! "$(ls -A $VUNDLE_DIR)" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
+  vim +PluginInstall +qall
+else
+  git --work-tree="$VUNDLE_DIR" --git-dir="$VUNDLE_DIR/.git" pull origin master
+fi
+
 ln -s "$DOTFILES_DIR/.bashrc" ~
 ln -s "$DOTFILES_DIR/.inputrc" ~
 ln -s "$DOTFILES_DIR/.vimrc" ~
